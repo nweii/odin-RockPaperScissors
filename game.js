2c29a336070a9d7f6b54c519ad39e0a723c5eb2a
Nathan <nthn.wei@gmail.com>
2022-12-26 20:14:23 +0000

    Created game script

---
+++ game.js
@@ -0,0 +1,81 @@
+function getComputerChoice() {
+	// set up choices
+	let choices = ['scissors', 'rock', 'paper'];
+	// console.log(choices);
+	
+	// pick random index between 0 and 2
+	let choiceNum = Math.floor(Math.random() * 3);
+	// console.log(choiceNum, choices[choiceNum]);
+	return choices[choiceNum]
+}
+
+function playRound(playerChoice, computerChoice) {
+	// set both inputs to lowercase
+	playerChoice = playerChoice.toLowerCase();
+	computerChoice = computerChoice.toLowerCase();
+	
+	// Compare choices and determine a winner.
+	if (playerChoice === computerChoice) {
+		return "It's a tie!";
+	}
+	else if (playerChoice === 'rock') {
+		if (computerChoice === 'scissors') {
+			return "You win! Rock beats Scissors."
+		} else {return "You lose! Paper beats Rock."}
+	}
+	else if (playerChoice === 'paper') {
+		if (computerChoice === 'rock') {
+			return "You win! Paper beats Rock."
+		} else {return "You lose! Scissors beats Paper."}
+	}
+	else if (playerChoice === 'scissors') {
+		if (computerChoice === 'paper') {
+			return "You win! Scissors beats Paper."
+		} else {return "You lose! Rock beats Scissors."}
+	}
+	else {
+		return "Invalid choices"
+	}
+}
+
+let playerChoice = "rock";
+const computerChoice = getComputerChoice();
+console.log(playRound(playerChoice, computerChoice));
+
+let playerScore = 0;
+let computerScore = 0;
+console.log(playerScore, computerScore);
+
+function game() {
+	// loop 5 rounds
+	
+	for (let i = 0; i < 5; i++) {
+		// prompt player for choice
+		playerChoice = prompt("Rock, Paper, or Scissors?");
+		console.log(playerChoice);
+
+		let roundResult = playRound(playerChoice, getComputerChoice());
+		
+		if (roundResult.includes('win')) {
+			playerScore++;
+			console.log(`${roundResult} (You: ${playerScore}, Computer: ${computerScore})`);
+		} 
+		else if (roundResult.includes('lose')) {
+			computerScore++;
+			console.log(`${roundResult} (You: ${playerScore}, Computer: ${computerScore})`);
+		}
+		else {
+			console.log(`${roundResult} (You: ${playerScore}, Computer: ${computerScore})`);
+		}
+	}
+	
+	if (playerScore > computerScore) {
+		return `You won by ${playerScore - computerScore} points! (${playerScore} v ${computerScore})`
+	}
+	else if (playerScore < computerScore) {
+		return `You lost by ${computerScore - playerScore} points. (${playerScore} v ${computerScore})`
+	}
+	else {
+		return "Tie match!"
+	}
+}
