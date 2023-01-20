3daf3a74af308a3dceb198d61198b7d281ab04b8
Nathan <nthn.wei@gmail.com>
2023-01-20 20:13:30 +0000

    Complete UI

--- game.js
+++ game.js
@@ -3,11 +3,7 @@ let playerChoice = 'rock';
 let enemyChoice = 'rock';
 console.log(`Your choice: ${playerChoice}, Opponent choice: ${enemyChoice}`);
 
-let roundNum = 1;
-let estRounds = 5;
-let prevRoundNum = roundNum - 1;
-let prevRoundSection;
-let newRoundSection;
+let roundNum = 0;
 
 let playerScore = 0;
 let enemyScore = 0;
@@ -21,20 +17,19 @@ let roundSign = document.querySelector("#roundSign");
 let steps = document.querySelector("#stepItems");
 
 // explanation text for the win/loss outcome.
-let whyOutcome = document.querySelector("#whyOutcome"); 
+let whyOutcome = document.querySelector("#whyOutcome");
 
 let playerHand = document.querySelector("#playerHand");
 let enemyHand = document.querySelector("#enemyHand");
 // the text on the table announcing WIN LOSE or TIE
-let winLose = document.querySelector("#winLose"); 
+let winLose = document.querySelector("#winLose");
 // the image for the table
-let glassImg = document.querySelector("#glassImg"); 
+let glassImg = document.querySelector("#glassImg");
 
 // the text containing player score, under the table
-let scoreLeft = document.querySelector("#scoreLeft"); 
+let scoreLeft = document.querySelector("#scoreLeft");
 // the text containing enemy score, under the table
-let scoreRight = document.querySelector("#scoreRight"); 
-
+let scoreRight = document.querySelector("#scoreRight");
 
 let instruction = document.querySelector("#instruction");
 
@@ -43,259 +38,285 @@ if ('ontouchstart' in window || navigator.maxTouchPoints) instruction.innerHTML 
 else instruction.innerHTML = "Click anywhere";
 
 // the parent element containing the choice buttons
-let choices = document.querySelector("#choices"); 
+let choices = document.querySelector("#choices");
+// the button for proceeding to the next round
+let next = document.querySelector("#nextRound");
+
 // the final result announcement text on game end.
-let final = document.querySelector("#final"); 
+let final = document.querySelector("#final");
 
-const blankRound = document.querySelector("#r1").cloneNode(true); // set aside a blank copy of this round for use in newRound()
+const blankRound = document.querySelector("#blankRound"); // set aside a blank copy of this round for use in newRound()
 
-playRound();
+nextRound(++roundNum);
+
+function nextRound(n) {
+	newRound(n);
+	playRound();
+}
+
+// for (playerScore, enemyScore; playerScore < 5 || enemyScore < 5;) {
+// 	for (let roundNum = 1; roundNum++;) {
+// 		newRound(roundNum);
+// 		console.log("--- ...newRound() complete. ---");
+// 		playRound();
+// 		console.log("--- ...playRound() complete. ---");
+// 	}
+// }
+// 
+// endGame();
+// console.log("--- Game ended. ---");
+
+
+function newRound(n) {
+	console.log(`--- Making newRound(${n})... ---`)
+
+	let roundSection = makeRound(n);
+
+	function makeRound(n) {
+		console.log("Building round...");
+		// copy new round section from template
+		let roundNode = document.getElementById("blankRound").cloneNode(true);
+		// name it after the round number
+		roundNode.id = `round-${n}`;
+
+		console.log(`...Built new round section #${roundNode.id}:`);
+		console.log(roundNode);
+		return roundNode;
+	}
+	// INSERT hidden new round
+	document.body.insertBefore(roundSection, final);
+	// RENAME
+	suffixChildIds(roundSection, n);
+
+	function suffixChildIds(parent, n) {
+		parent.querySelectorAll('*').forEach(function(child) {
+			child.id += `-${n}`;
+		});
+	}
+	// SHOW new round
+	roundSection.classList.remove("hidden");
+	console.log("Added new round section to document.");
+	window.scrollTo(0, document.body.scrollHeight);	
+	
+	// RIG variables to new round IDs
+	roundSign = document.querySelector(`#roundSign-${n}`);
+	steps = document.querySelector(`#stepItems-${n}`);
+	stepItems = document.querySelectorAll(`#stepItems-${n} span`);
+	whyOutcome = document.querySelector(`#whyOutcome-${n}`);
+	playerHand = document.querySelector(`#playerHand-${n}`);
+	enemyHand = document.querySelector(`#enemyHand-${n}`);
+	winLose = document.querySelector(`#winLose-${n}`);
+	glassImg = document.querySelector(`#glassImg-${n}`);
+	scoreLeft = document.querySelector(`#scoreLeft-${n}`);
+	scoreRight = document.querySelector(`#scoreRight-${n}`);
+	instruction = document.querySelector(`#instruction-${n}`);
+	choices = document.querySelector(`#choices-${n}`);
+	next = document.querySelector(`#nextRound-${n}`);
+	// LOAD scoreboard
+	scoreLeft.textContent = playerScore;
+	scoreRight.textContent = enemyScore;
+	console.log("Rigged new variables & Loaded scoreboard.")
+
+	// LOAD Round # banner
+	roundSign.textContent = `Round ${n}`;
+	console.log(`Updated round sign.`);
+}
 
 function playRound() {
-	console.log("Starting playRound()");
+	console.log("--- Starting playRound()... ---");
 	
-	// each span element in "Rock…Paper…Scissors…Shoot!"
-	let stepItems = document.querySelectorAll("#stepItems span"); 
-	// reset click count as playRound() will be called at the start of each new round.
+	// Get elements in "Rock…Paper…Scissors…Shoot!"
+	let stepItems = document.querySelectorAll(`#stepItems-${roundNum} span`);
 	clickCount = 0;
-	console.log(`clickCount reset to ${clickCount}.`);
+	console.log(`Clicks set to ${clickCount}.`);
 	
-	// increment clickCount on release
+	// Clicks increment on release
 	document.addEventListener('mouseup', addClick);
+	
 	function addClick() {
 		clickCount++;
 		console.log(`${clickCount} clicks`);
 	}
 	
-	// on each click down, Animate hands down and light a chant. If 
-	document.addEventListener('mousedown', animDown);
-	console.log("handsdown and chanting activated.")
+	// Hands go down and RPS blinks on click down
+	document.addEventListener('mousedown', down);
+	// Hands reset on click release
+	document.addEventListener('mouseup', up);
 	
-	document.addEventListener('mouseup', animUp);
-	console.log("handsup activated.");
-	
-	function animDown() {
+	function down() {
 		// Animate both hands downward
 		playerHand.classList.add('rotate-12', 'translate-y-2.5');
 		enemyHand.classList.add('-rotate-12', 'translate-y-2.5');
-		
-		lightStep();
-	}
 	
-	function lightStep() {
 		// Brighten "rock" then "paper" then "scissors"
 		stepItems[clickCount].classList.remove("opacity-50");
 	}
 	
-	function animUp() {
+	function up() {
 		// Reset hands back upward
 		playerHand.classList.remove('rotate-12', 'translate-y-2.5');
 		enemyHand.classList.remove('-rotate-12', 'translate-y-2.5');
 		
-		// after last animUp / after "Scissors…" lights up
-		if (clickCount === 3) {
+		if (clickCount >= 3) {
 			showChoices();
 		}
 	}
-	
+
 	function showChoices() {
+		// ENABLE button clicks
 		choices.addEventListener("click", choose);
-		console.log("Added choice click event listener.");
-		
-		// remove step/animation click anywhere behaviors
-		document.removeEventListener('mousedown', animDown);
-		console.log("chanting deactivated.");
+		console.log("Buttons activated.");
+
+		// REMOVE chant behaviors
+		document.removeEventListener('mousedown', down);
 		document.removeEventListener('mouseup', addClick);
-		console.log("Click counts deactivated.");
-		// hide "Tap/click anywhere"
-		instruction.classList.add("hidden"); 
-		
-		// reveal choices
+		document.removeEventListener('mouseup', up);
+		console.log("No more chanting.");
+		// HIDE "Tap/click anywhere"
+		instruction.classList.add("hidden");
+
+		// REVEAL choices
 		choices.classList.remove("hidden");
-		console.log("showing choices");
-		// Save choice button text as player choice when clicked
-		document.removeEventListener('mousedown', animUp);
+		console.log("Choices revealed.");
 	}
 
 	function choose(e) {
+		// SAVE choice button text as player choice when clicked
 		playerChoice = e.target.textContent;
 		console.log(`You chose ${playerChoice}`);
-		choices.classList.add("hidden"); // hide choices again
-		stepItems[clickCount].classList.remove("opacity-50"); // un-fade "Shoot!" aka index 3 of #stepItems
+		// HIDE choices
+		choices.classList.add("hidden");
+		// Brighten "Shoot!" aka index 3 of #stepItems
+		stepItems[clickCount].classList.remove("opacity-50");
+		// DISABLE button clicks
+		choices.removeEventListener("click", choose);
+		console.log("Buttons deactivated.");
 		getOutcome();
 	}
-	
+
 	function getOutcome() {
-		// set both inputs to lowercase
 		playerChoice = playerChoice.toLowerCase();
 		enemyChoice = getComputerChoice().toLowerCase();
 		console.log(`Enemy chose ${enemyChoice}`);
+		
 		updateHands();
+		function updateHands() {
+			switch (playerChoice) {
+				case 'rock':
+				playerHand.src = "/assets/Player-Rock.png";
+				break;
+				case 'paper':
+				playerHand.src = "/assets/Player-Paper.png";
+				break;
+				case 'scissors':
+				playerHand.src = "/assets/Player-Scissors.png";
+				break;
+			}
+			
+			switch (enemyChoice) {
+				case 'rock':
+				enemyHand.src = "/assets/Opponent-Rock.png";
+				break;
+				case 'paper':
+				enemyHand.src = "/assets/Opponent-Paper.png";
+				break;
+				case 'scissors':
+				enemyHand.src = "/assets/Opponent-Scissors.png";
+				break;
+			}
+		}
 		
 		// Compare choices and determine a winner.
 		if (playerChoice === enemyChoice) {
 			whyOutcome.textContent = "Love is love<3";
 			endRound('tie');
-		}
-		else if (playerChoice === 'rock') {
+		} else if (playerChoice === 'rock') {
 			if (enemyChoice === 'scissors') {
-				whyOutcome.textContent = "Rock breaks Scissors";
+				whyOutcome.innerHTML = '<span class="font-semibold">Rock</span> breaks <span class="font-semibold">Scissors</span>';
 				endRound('win');
-			} 
-			else {
-				whyOutcome.textContent = "Paper covers Rock";
+			} else {
+				whyOutcome.innerHTML = '<span class="font-semibold">Paper</span> covers <span class="font-semibold">Rock</span>';
 				endRound('loss');
 			}
-		}
-		else if (playerChoice === 'paper') {
+		} else if (playerChoice === 'paper') {
 			if (enemyChoice === 'rock') {
-				whyOutcome.textContent = "Paper covers Rock";
+				whyOutcome.innerHTML = '<span class="font-semibold">Paper</span> covers <span class="font-semibold">Rock</span>';
 				endRound('win');
-			}
-			else {
-				whyOutcome.textContent = "Scissors cut Paper";
+			} else {
+				whyOutcome.innerHTML = '<span class="font-semibold">Scissors</span> cut <span class="font-semibold">Paper</span>';
 				endRound('loss');
 			}
-		}
-		else if (playerChoice === 'scissors') {
+		} else if (playerChoice === 'scissors') {
 			if (enemyChoice === 'paper') {
-				whyOutcome.textContent = "Scissors cut Paper";
+				whyOutcome.innerHTML = '<span class="font-semibold">Scissors</span> cut <span class="font-semibold">Paper</span>';
 				endRound('win');
-			}
-			else {
-				whyOutcome.textContent = "Rock breaks Scissors";
+			} else {
+				whyOutcome.innerHTML = '<span class="font-semibold">Rock</span> breaks <span class="font-semibold">Scissors</span>';
 				endRound('loss');
 			}
-		}
-		else {
+		} else {
 			return "Invalid choices"
 		}
 		
-		function endRound(outcome) {
-			glassImg.src = `/assets/table-${outcome}.svg`;
-			winLose.textContent = `${outcome}`;
-			whyOutcome.classList.remove("hidden");
-			winLose.classList.remove("hidden");
-			steps.classList.add("hidden");
+		function getComputerChoice() {
+			// set up choices
+			let choices = ['rock', 'paper', 'scissors'];
 			
-			switch (outcome) {
-				case 'win':
+			// pick random index between 0 and 2
+			let choiceNum = Math.floor(Math.random() * 3);
+			return choices[choiceNum]
+		}
+	}
+
+	function endRound(outcome) {
+		// UPDATE table
+		glassImg.src = `/assets/table-${outcome}.svg`;
+		winLose.textContent = `${outcome}`;
+		// DIM round sign
+		roundSign.classList.add("opacity-40");
+		roundSign.classList.remove("font-bold");
+		roundSign.classList.remove("text-amber-300");
+		// REVEAL result text
+		whyOutcome.classList.remove("hidden");
+		winLose.classList.remove("hidden");
+		// HIDE chant text
+		steps.classList.add("hidden");
+		// SHORTEN bottom margin
+		if (roundNum) {document.getElementById(`round-${roundNum}`).classList.add("mb-1");
+		document.getElementById(`round-${roundNum}`).classList.remove("mb-48");}
+		
+		// UPDATE scores
+		// SHOW scores
+		switch (outcome) {
+			case 'win':
 				playerScore++;
 				scoreLeft.textContent = playerScore;
 				break;
-				case 'loss':
+			case 'loss':
 				enemyScore++;
 				scoreRight.textContent = enemyScore;
 				break;
-				case 'tie':
-				estRounds++;
-				updateRoundLabel();
+			case 'tie':
 				break;
-			}
-			
-			if (playerScore === 5 || enemyScore === 5) {
-				final.classList.remove("hidden");
-				if (playerScore > enemyScore) {
-					final.textContent = `YOU WON!`;
-					document.body.classList.add("bg-[#063217]");
-					document.body.classList.remove("bg-black");
-				} 
-				else {
-					final.textContent = `YOU LOST!`;
-					document.body.classList.add("bg-[#400202]");
-					document.body.classList.remove("bg-black");
-				};
-			}
-			else newRound();
 		}
+		
+		if (playerScore === 5 || enemyScore === 5) {
+			endGame();
+		}
+		else nextRound(++roundNum);
 	}
 }
 
-function newRound() {
-	roundNum++;
-	console.log(`--- STARTING ROUND ${roundNum} ---`);
-	// We need to numerate the previous IDs because everything in this script works only un-numerated section IDs. We also need to avoid creating duplicate IDs.
-	prevRoundNum = roundNum - 1;
-	console.log(`prevRoundNum is ${prevRoundNum}`);
-	prevRoundSection = document.getElementById(`r${prevRoundNum}`);
-	console.log("Previous round section:"); console.log(prevRoundSection);
-	suffixChildIds(prevRoundSection, prevRoundNum);
-	
-	// create new section for this round
-	newRoundSection = blankRound;
-	newRoundSection.id = "r" + roundNum; // name it after the round number
-	console.log(`Created new round section #${newRoundSection.id}:`); console.log(newRoundSection);
-	
-	// reveal the next round
-	document.body.insertBefore(newRoundSection, final);
-	console.log("Added new round section to document.");
-	
-	refreshVars();
-	updateRoundLabel();
-	playRound();
-	
-	function suffixChildIds(parent, number) {
-		parent.querySelectorAll('*').forEach(function (child) {
-			child.id += number;
-		});
-	}
+function endGame() {
+	final.classList.remove("hidden");
+	if (playerScore > enemyScore) {
+		final.textContent = `YOU WON!`;
+		document.body.classList.add("bg-[#063217]");
+		document.body.classList.remove("bg-black");
+	} 
+	else if (playerScore < enemyScore) {
+		final.textContent = `YOU LOST!`;
+		document.body.classList.add("bg-[#400202]");
+		document.body.classList.remove("bg-black");
+	} else console.log("Somehow tie game");
 }
-
-function refreshVars() {
-	roundSign = document.querySelector("#roundSign");
-	steps = document.querySelector("#stepItems");
-	stepItems = document.querySelectorAll("#stepItems span"); 
-	whyOutcome = document.querySelector("#whyOutcome"); 
-	playerHand = document.querySelector("#playerHand");
-	enemyHand = document.querySelector("#enemyHand");
-	winLose = document.querySelector("#winLose"); 
-	glassImg = document.querySelector("#glassImg"); 
-	scoreLeft = document.querySelector("#scoreLeft"); 
-	scoreRight = document.querySelector("#scoreRight"); 
-	instruction = document.querySelector("#instruction");
-	choices = document.querySelector("#choices");
-	
-	scoreLeft.textContent = playerScore;
-	scoreRight.textContent = enemyScore;
-	console.log(`Player: ${playerScore}, Enemy: ${enemyScore}`)
-}
-
-function getComputerChoice() {
-	// set up choices
-	let choices = ['rock', 'paper', 'scissors'];
-	
-	// pick random index between 0 and 2
-	let choiceNum = Math.floor(Math.random() * 3);
-	return choices[choiceNum]
-}
-
-function updateHands() {
-	switch (playerChoice) {
-		case 'rock':
-		playerHand.src = "/assets/Player-Rock.png";
-		break;
-		case 'paper':
-		playerHand.src = "/assets/Player-Paper.png";
-		break;
-		case 'scissors':
-		playerHand.src = "/assets/Player-Scissors.png";
-		break;
-	}
-	
-	switch (enemyChoice) {
-		case 'rock':
-		enemyHand.src = "/assets/Opponent-Rock.png";
-		break;
-		case 'paper':
-		enemyHand.src = "/assets/Opponent-Paper.png";
-		break;
-		case 'scissors':
-		enemyHand.src = "/assets/Opponent-Scissors.png";
-		break;
-	}
-}
-
-function updateRoundLabel() {
-	roundSign.innerHTML = `<span class="font-bold">Round ${roundNum}</span>/${estRounds}`;
-	console.log(`Updated round sign.`)
-};
